859823f002c4f7b93d75001a8147028f8ebd435b
jonesim <ian@cobi.co.uk>
2021-08-24 13:40:08 +0100

    Up version to 0.0.11

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as fh:
 
 setuptools.setup(
     name="django-ajax-helpers",
-    version="0.0.10",
+    version="0.0.11",
     author="Ian Jones",
     description="Django app to assist with posting Ajax and receiving commands from django",
     long_description=long_description,
