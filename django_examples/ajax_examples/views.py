70b954d44b6468870820cde9c4e11cd37eb10fd3
jonesim <ian@cobi.co.uk>
2021-12-20 20:27:34 +0000

    Separated examples into different pages Added a couple of examples

--- django_examples/ajax_examples/views.py
+++ django_examples/ajax_examples/views.py
@@ -1,22 +1,30 @@
+import base64
 import datetime
+import os
 from io import BytesIO
 
 from django import forms
 from django.http import HttpResponse
-from django.views.generic import TemplateView
 from django.urls import reverse
-from openpyxl import Workbook
-
-from ajax_helpers.mixins import AjaxHelpers, ReceiveForm, AjaxFileHelpers
+from django.views.generic import TemplateView
 from django_menus.menu import MenuMixin
+from openpyxl import Workbook
 from show_src_code.view_mixins import DemoViewMixin
+
+from ajax_helpers.mixins import AjaxHelpers, ReceiveForm, AjaxFileHelpers, AjaxFileUploadMixin
+from ajax_helpers.utils import ajax_command
 
 
 class MainMenu(DemoViewMixin, MenuMixin):
 
     def setup_menu(self):
         super().setup_menu()
-        self.add_menu('main_menu').add_items('ajax_main')
+        self.add_menu('main_menu').add_items(
+            ('ajax_main', 'General'),
+            ('timer_examples', 'Timers'),
+            ('download_examples', 'File Downloads'),
+            ('dragdrop_upload', 'File Uploads'),
+        )
 
 
 class TestForm(forms.Form):
@@ -39,17 +47,17 @@ class ToolTip(TemplateView):
 
 class Example1(MainMenu, AjaxFileHelpers, ReceiveForm, AjaxHelpers, TemplateView):
 
-    template_name = 'ajax_examples/base.html'
+    template_name = 'ajax_examples/main.html'
 
-    def button_redirect(self, **kwargs):
+    def button_redirect(self):
         self.add_command('message', text='Will redirect after this message')
         self.add_command('redirect', url=reverse('redirect'))
         return self.command_response()
 
-    def button_test_ajax(self, **kwargs):
+    def button_test_ajax(self):
         return self.command_response('message', text='From Django View')
 
-    def button_test_html(self, **kwargs):
+    def button_test_html(self):
         return self.command_response('html', selector='#html_test', html='From Django View')
 
     def form_form_id(self, **kwargs):
@@ -62,39 +70,16 @@ class Example1(MainMenu, AjaxFileHelpers, ReceiveForm, AjaxHelpers, TemplateView
         else:
             return self.command_response('html', selector='#django_form_id', html=a.as_p())
 
-    @staticmethod
-    def button_download_text(**kwargs):
-        response = HttpResponse(content_type='text/plain')
-        response['Content-Disposition'] = 'attachment; filename="text.txt"'
-        response.write('text file data')
-        return response
+    def button_append(self):
+        self.add_command('append_to', html='<div>New div</div>', element='#append-to')
+        return self.command_response()
 
-    @staticmethod
-    def button_download_blob(**kwargs):
-        workbook = Workbook()
-        sheet = workbook.active
-        sheet.append(['hello', 'world'])
-        filename = 'test.xlsx'
-        output = BytesIO()
-        workbook.save(output)
-        output.seek(0)
-        response = HttpResponse(content_type='application/ms-excel')
-        # Providing extra download information for the user's browser.
-        response['Content-Disposition'] = 'attachment; filename="%s"' % filename
-        response.write(output.read())
-        return response
+    def button_css(self):
+        self.add_command('set_css', selector='#css-test', prop='width', val='800px')
+        return self.command_response('set_css', selector='#css-test', prop='background-color', val='yellow')
 
-    def button_download_base64(self, **kwargs):
-        workbook = Workbook()
-        sheet = workbook.active
-        sheet.append(['hello', 'world'])
-        filename = 'test_base64.xlsx'
-        output = BytesIO()
-        workbook.save(output)
-        output.seek(0)
-        import base64
-        return self.command_response('save_file', data=base64.b64encode(output.read()).decode('ascii'),
-                                     filename=filename)
+    def button_null(self):
+        return self.command_response('null')
 
     def file_upload(self, file):
         return self.command_response('message', text=f'Received {file.name} size {file.size}')
@@ -107,11 +92,98 @@ class Example1(MainMenu, AjaxFileHelpers, ReceiveForm, AjaxHelpers, TemplateView
     def add_to_context(self, **kwargs):
         return {'title': type(self).__name__, 'filter': filter}
 
-    def timer_demo_interval(self, **_kwargs):
-        return self.command_response('html', selector='#time_div',
-                                     html=datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S'))
-
 
 class Example2(Example1):
 
     template_name = 'ajax_examples/redirect.html'
+
+
+class TimerExample(Example1):
+    template_name = 'ajax_examples/timer.html'
+
+    def timer_test(self, **kwargs):
+        return self.command_response('html', selector='#time_div',
+                                     html=datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S'))
+
+    def get_context_data(self, **kwargs):
+
+        self.add_page_command(
+            'timeout', commands=[ajax_command('message', text='Message appears after 4 seconds')], time=4000
+        )
+        self.add_page_command(
+            'timer', commands=[ajax_command('ajax_post', url=self.request.path, data={'timer': 'test'})], interval=2000
+        )
+
+        return super().get_context_data(**kwargs)
+
+
+class DownloadExamples(Example1):
+    template_name = 'ajax_examples/file_downloads.html'
+
+    @staticmethod
+    def create_excel_file():
+        workbook = Workbook()
+        sheet = workbook.active
+        sheet.append(['hello', 'world'])
+        output = BytesIO()
+        workbook.save(output)
+        output.seek(0)
+        return output
+
+    def button_redirect_download(self, download=None):
+        return self.command_response('redirect', url=reverse('download_examples') + f'?download={download}')
+
+    @staticmethod
+    def button_download_text():
+        response = HttpResponse(content_type='text/plain')
+        response['Content-Disposition'] = 'attachment; filename="text.txt"'
+        response.write('text file data')
+        return response
+
+    def button_download_blob(self):
+        filename = 'test.xlsx'
+        excel_file = self.create_excel_file()
+        response = HttpResponse(content_type='application/ms-excel')
+        # Providing extra download information for the user's browser.
+        response['Content-Disposition'] = 'attachment; filename="%s"' % filename
+        response.write(excel_file.read())
+        return response
+
+    def button_download_base64(self, **kwargs):
+        excel_file = self.create_excel_file()
+        filename = 'test_base64.xlsx'
+        return self.command_response('save_file', data=base64.b64encode(excel_file.read()).decode('ascii'),
+                                     filename=filename)
+
+    def button_download_pdf(self):
+        f = open('/app/sample.pdf', 'rb')
+        return self.command_response('save_file', data=base64.b64encode(f.read()).decode('ascii'),
+                                     filename='sample.pdf', type='application/pdf')
+
+    def get(self, request, *args, **kwargs):
+        download = request.GET.get('download')
+        if download in ['1', '2']:
+            download_type = 'attachment' if download == '1' else 'inline'
+            f = open('/app/sample.pdf', 'rb')
+            response = HttpResponse(content_type='application/pdf')
+            response['Content-Disposition'] = f"{download_type}; filename=test.pdf"
+            response.write(f.read())
+            f.close()
+            return response
+        return super().get(request, *args, **kwargs)
+
+
+class DragDropUpload(AjaxFileUploadMixin, Example1):
+
+    template_name = 'ajax_examples/dragdrop_upload.html'
+
+    @staticmethod
+    def upload_files(filename, _size, file, **kwargs):
+        path = '/media/' + filename
+        with open(path, 'wb+') as destination:
+            destination.write(file.read())
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['files'] = os.listdir('/media/')
+        return context
--- django_examples/django_examples/settings.py
+++ django_examples/django_examples/settings.py
@@ -128,3 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = '/media/'
+MEDIA_URL = '/media/'
