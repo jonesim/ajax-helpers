16addd7bc58ea756a030d7a421d7675aa50b0b7e
jonesim <ian@cobi.co.uk>
2021-12-20 20:20:20 +0000

    Remove function name from kwargs Add AjaxFileUploadMixin

--- ajax_helpers/mixins.py
+++ ajax_helpers/mixins.py
@@ -16,7 +16,7 @@ class AjaxHelpers:
         super().__init__(*args, **kwargs)
         self.command_set = set()
         for c in inspect.getmro(self.__class__):
-            self.command_set = self.command_set | set(getattr(c,'ajax_commands', []))
+            self.command_set = self.command_set | set(getattr(c, 'ajax_commands', []))
         self.response_commands = []
         self.page_commands = []
 
@@ -36,13 +36,15 @@ class AjaxHelpers:
             if request.content_type == 'application/json':
                 response = json.loads(request.body)
             elif request.content_type == 'multipart/form-data':
-                response = request.POST
+                response = request.POST.dict()
             else:
                 response = None
             if response:
                 for t in self.command_set:
                     if t in response and hasattr(self, f'{t}_{response[t]}'):
-                        return getattr(self, f'{t}_{response[t]}')(**response)
+                        method = getattr(self, f'{t}_{response[t]}')
+                        del response[t]
+                        return method(**response)
         if hasattr(super(), 'post'):
             return super().post(request, *args, **kwargs)
 
@@ -95,3 +97,45 @@ class AjaxFileHelpers(AjaxHelpers):
         if request.is_ajax() and request.content_type == 'multipart/form-data' and 'ajax_file' in request.FILES:
             return self.file_upload(request.FILES['ajax_file'])
         return super().post(request, *args, **kwargs)
+
+
+class AjaxFileUploadMixin:
+    ajax_commands = ['start_upload', 'upload']
+    upload_key = 'ajax_modal_file'
+    single_progress_bar = True
+
+    def progress_bar_id(self, index):
+        if self.single_progress_bar:
+            index = 0
+        return f'#file_progress_bar_{index}'
+
+    def post(self, request, *args, **kwargs):
+        if request.is_ajax() and request.content_type == 'multipart/form-data' and self.upload_key in request.FILES:
+            response = request.POST.dict()
+            file = self.request.FILES[self.upload_key]
+            upload_params = json.loads(response.get('upload_params', '{}'))
+            index = int(response.pop('index'))
+            file_info = [f for f in json.loads(response['file_info']) if f['size'] > 0]
+            getattr(self, 'upload_' + request.POST['upload'])(file_info[index]['name'], file_info[index]['size'], file,
+                                                              **upload_params)
+            if len(file_info) > index + 1:
+                response['upload_params'] = upload_params
+                return self.command_response('upload_file', **self.upload_file_command(index + 1, **response))
+            return self.upload_completed()
+        return super().post(request, *args, **kwargs)
+
+    def upload_file_command(self, index, **kwargs):
+        upload_file = {k: v for k, v in kwargs.items() if k in ['upload_params', 'drag_drop', 'selector']}
+        upload_file.update({'options': {'progress': {'selector': self.progress_bar_id(index)}}, 'index': index})
+        return upload_file
+
+    def start_upload_files(self, **kwargs):
+        kwargs['files'] = [f for f in kwargs['files'] if f['size'] > 0]
+        if len(kwargs['files']) == 0:
+            return self.upload_completed()
+        return self.command_response('upload_file', **self.upload_file_command(0, **kwargs))
+
+    def upload_completed(self):
+        self.add_command('delay', time=500)
+        self.add_command('message', text='Download complete')
+        return self.command_response('reload')
