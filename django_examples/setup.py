f8a1f64926c2b82c4adc8cc3fa9976cb3375a7b6
jonesim <ian@cobi.co.uk>
2021-12-29 17:39:24 +0000

    Example requires openpyxl

--- django_examples/setup.py
+++ django_examples/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="ajax-examples",
-    version="0.0.1",
+    version="0.0.2",
     author="Ian Jones",
     description="Ajax examples",
     include_package_data = True,
@@ -13,4 +13,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=['openpyxl>=3.0.7'],
 )
